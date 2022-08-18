0910aafcf259371c58e088e1d984f28f00233a99
Marat Jilikbaev <jilikbaevmarat93@gmail.com>
2022-08-18 20:35:15 +0600

    added data/interier photos and main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def convert_picture_to_color_vector(picture_obj):
+
+    # how to get to pixels?
+    rgb_picture_vector = 1
+    return rgb_picture_vector
+
+if __name__ == "__main__":
+    print(8)
+    a = 3
