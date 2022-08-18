3fded52885a2d3d9c87edafc4c9e2586ef91642d
Marat Jilikbaev <jilikbaevmarat93@gmail.com>
2022-08-18 20:57:02 +0600

    added requirements.txt and git ignore

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 def convert_picture_to_color_vector(picture_obj):
 
     # how to get to pixels?
@@ -5,5 +7,10 @@ def convert_picture_to_color_vector(picture_obj):
     return rgb_picture_vector
 
 if __name__ == "__main__":
-    print(8)
-    a = 3
+    from collections import Counter
+    from sklearn.cluster import KMeans
+    from matplotlib import colors
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import cv2
+    import pandas as pd
